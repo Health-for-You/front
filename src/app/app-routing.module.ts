570a336a82646ce688d909086fedaa82b0166f18
gabriit01 <gabriit01@gmail.com>
2023-02-27 21:41:54 +0100

    [ADD]ed routing + router outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { LoginComponent } from 'src/auth/login/login.component';
+import { RoutesService } from './@shared/services/routes.service';
+const routes: Routes = [
+  {path:'login', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
